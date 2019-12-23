0e925e54270bf002dfd20aff45bab9267f68a52c
Jeremy Marotta <firefreet@gmail.com>
2019-12-23 18:50:10 -0500

    adjust current hour gradient at start and end of hour. remove iterations of html calendar rows and replace with clones in a for loop of js script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready( function(){
+$(document).ready(function () {
     function c(x) {
         console.log(x);
     };
@@ -7,41 +7,59 @@ $(document).ready( function(){
     var thisDateString = thisDate.format("YYYY-MM-DD");
     var aptString = "appointments" + thisDateString;
     var thisDayData = {}
-    var cleanDate = {'hour':"0",'minute':'0','second':'0','millisecond':'0'}
+    var cleanDate = { 'hour': "0", 'minute': '0', 'second': '0', 'millisecond': '0' }
     var percentOfHour = 0
-    var percentToGo = 0
+    var calRow = $("div.row.my-1")
+
+    // clone calendar row for hours 10AM to 5PM. 
+    // set data-hour and text value for consective hours
+    for (var i = 10; i < 18; i++) {
+        var newCalRow = calRow.clone();
+        newCalRow.attr("data-hour", i);
+        // transform 
+        var v = moment().hour(i).format("hh A")
+        c(v);
+        newCalRow.children("div.hour-displ").text(v);
+        $("div.container>div.row>div.col").append(newCalRow);
+    }
 
     // set listeners
     $(".btn>.fa-lg").parent().on("click", save);
     $("textarea").on("input", checkValue);
     $(".fa-arrow-left,.fa-arrow-right").parent().on("click", switchDay);
-    $("#today").on("click",switchDay);
+    $("#today").on("click", switchDay);
 
     // check current time once per second to update the background colors
     // of the textarea fields
-    var i = setInterval( function() {
+    var i = setInterval(function () {
         // background based on if the hour has passed, is current or is future
-        $("textarea").each( function(){
-            percentOfHour = (((moment().minute() * 60) + moment().second()) / 3600) * 100;
-            percentOfHour = parseFloat(percentOfHour.toFixed(5));
-            greenPercent = Math.min(percentOfHour + 6,99.9999);
+        $("textarea").each(function () {
             var hour = moment().hour();
-            if((thisDate.format("YYYYMMDD") === moment().format("YYYYMMDD") && parseInt($(this).parent().data("hour")) < hour) || thisDate.set(cleanDate).isBefore(moment().set(cleanDate))) {
-                $(this).attr("style","background-color: lightgray;");
-            } else if ((thisDate.format("YYYYMMDD") === moment().format("YYYYMMDD") && parseInt($(this).parent().data("hour")) === hour)) {
+            // set grey if day or hour is passed
+            if ((thisDate.format("YYYYMMDD") === moment().format("YYYYMMDD") && parseInt($(this).parent().data("hour")) < hour) || thisDate.set(cleanDate).isBefore(moment().set(cleanDate))) {
+                $(this).attr("style", "background-color: lightgray;");
+            }
+            // for current hour of current day, set gradient from gray to green (with a red bar at the percentage of the hour past)
+            else if ((thisDate.format("YYYYMMDD") === moment().format("YYYYMMDD") && parseInt($(this).parent().data("hour")) === hour)) {
+                percentOfHour = (((moment().minute() * 60) + moment().second()) / 3600) * 100;
+                percentOfHour = Math.min(parseFloat(percentOfHour.toFixed(5)), 97);
+                if (percentOfHour < 3) { greenPercent = percentOfHour * 3; redPercent = percentOfHour * 1.5; }
+                else { greenPercent = Math.min(percentOfHour + 6, 100); redPercent = percentOfHour + 3; };
                 var style = [
-                    'background: -webkit-linear-gradient(left, lightgray ' + percentOfHour + '%, crimson, lightgreen ' + greenPercent + '%);',
-                    'background: -moz-linear-gradient(left, lightgray ' + percentOfHour + '%, crimson, lightgreen ' + greenPercent + '%);',
-                    'background: -ms-linear-gradient(left, lightgray ' + percentOfHour + '%, crimson, lightgreen ' + greenPercent + '%);',
-                    'background: linear-gradient(left, lightgray ' + percentOfHour + '%, crimson, lightgreen ' + greenPercent + '%);',
-                    'background: -o-linear-gradient(left,lightgray ' + percentOfHour + '%, crimson, lightgreen ' + greenPercent + '%);',
+                    'background: -webkit-linear-gradient(left, lightgray ' + percentOfHour + '%, crimson ' + redPercent + '%, lightgreen ' + greenPercent + '%);',
+                    'background: -moz-linear-gradient(left, lightgray ' + percentOfHour + '%, crimson ' + redPercent + '%, lightgreen ' + greenPercent + '%);',
+                    'background: -ms-linear-gradient(left, lightgray ' + percentOfHour + '%, crimson ' + redPercent + '%, lightgreen ' + greenPercent + '%);',
+                    'background: linear-gradient(left, lightgray ' + percentOfHour + '%, crimson ' + redPercent + '%, lightgreen ' + greenPercent + '%);',
+                    'background: -o-linear-gradient(left,lightgray ' + percentOfHour + '%, crimson ' + redPercent + '%, lightgreen ' + greenPercent + '%);',
                 ].join(";")
                 $(this).attr("style", style);
-            } else {
-                $(this).attr("style","background-color: lightgreen;");
+            }
+            // set green for future days / hours
+            else {
+                $(this).attr("style", "background-color: lightgreen;");
             }
         });
-    },100)
+    }, 100)
 
     // if local data exists, set it to an object variable
     if (localStorage.getItem("calendarData") !== null) {
@@ -49,15 +67,15 @@ $(document).ready( function(){
     };
 
     // hide the 'Go to Today' button
-    $("#today").css("visibility","hidden");
-    
+    $("#today").css("visibility", "hidden");
+
     setPage();
     // populates the page based on any existing saved data
     // and sets the buttons to Saved (locked)
     function setPage() {
         if (thisDate.format("YYYYMMDD") === moment().format("YYYYMMDD")) {
             thisDateString = thisDateString + " (Today)"
-        } 
+        }
         $(".display-date").text(thisDateString);
         $("button.btn>i.fa-lg").addClass("fa-lock");
         $(".row textarea").val("");
@@ -65,8 +83,8 @@ $(document).ready( function(){
             aptObj[aptString] = {}
         }
         thisDayData = aptObj[aptString];
-        $.each(thisDayData, function(k,v) {
-            $(".row[data-hour=\"" + k +"\"] textarea").val(v);
+        $.each(thisDayData, function (k, v) {
+            $(".row[data-hour=\"" + k + "\"] textarea").val(v);
         });
     };
 
@@ -78,7 +96,7 @@ $(document).ready( function(){
             bt.addClass("btn-light").removeClass("btn-dark disabled");
             bt.children("i").addClass("fa-unlock").removeClass("fa-lock");
         } else {
-            bt.addClass("btn-dark disabled").removeClass("btn-light"); 
+            bt.addClass("btn-dark disabled").removeClass("btn-light");
             bt.children("i").addClass("fa-lock").removeClass("fa-unlock");
         };
     };
@@ -88,7 +106,7 @@ $(document).ready( function(){
         var thisHour = $(this).parent().data("hour");
         thisDayData[thisHour] = $(this).siblings("textarea").val();
         aptObj[aptString] = thisDayData;
-        localStorage.setItem("calendarData",JSON.stringify(aptObj));
+        localStorage.setItem("calendarData", JSON.stringify(aptObj));
         $(this).addClass("btn-dark disabled").removeClass("btn-light");
         $(this).children("i").addClass("fa-lock").removeClass("fa-unlock");
     };
@@ -96,17 +114,17 @@ $(document).ready( function(){
     // increments the day forward or back and reset the page
     function switchDay() {
         if ($(this).children(".fa-arrow-right").length) {
-            thisDate = thisDate.add('1','d');
+            thisDate = thisDate.add('1', 'd');
         } else if ($(this).attr("id") === "today") {
             thisDate = moment();
-        } else {         
-            thisDate = thisDate.subtract('1','d');    
+        } else {
+            thisDate = thisDate.subtract('1', 'd');
         };
         // display the 'go to today' button if any other date is displayed
-        if(thisDate.format("YYYYMMDD") === moment().format("YYYYMMDD")) {
-            $("#today").css("visibility","hidden");
-        } else { 
-            $("#today").css("visibility","visible");
+        if (thisDate.format("YYYYMMDD") === moment().format("YYYYMMDD")) {
+            $("#today").css("visibility", "hidden");
+        } else {
+            $("#today").css("visibility", "visible");
         };
         thisDateString = thisDate.format("YYYY-MM-DD");
         aptString = "appointments" + thisDateString;
